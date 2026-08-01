20a202d3942ee6d4fb0738f623c497910855878f
hakimfreefire2008-a11y <hakilfreefire2008@gmail.com>
2026-08-01 01:27:46 +0100

    Connect JSON server database

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,25 +4,22 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
 
-
   static const String url =
-      "ضع_رابط_servers.json_هنا";
+  "https://raw.githubusercontent.com/hakimfreefire2008-a11y/CONFIGES-ABDELLALI-PRO/master/servers.json";
 
 
-  static Future<Map<String,dynamic>> getServers() async {
+  static Future<Map<String, dynamic>> getServers() async {
 
-
-    final response =
-    await http.get(
+    final response = await http.get(
       Uri.parse(url),
     );
 
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
 
       return jsonDecode(response.body);
 
-    }else{
+    } else {
 
       throw Exception(
         "Failed loading servers"
